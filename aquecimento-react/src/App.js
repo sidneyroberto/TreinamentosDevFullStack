54499cd32538852535ac811af3a4f4c313962692
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-09-02 10:32:19 -0400

    commit inicial

---
+++ aquecimento-react/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="menu">
+          <h1>Aquecimento</h1>
+        </div>
+
+        <div className="conteiner">
+          <h3>Aquecimento React</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
